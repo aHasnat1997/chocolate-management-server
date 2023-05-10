3884505f7aef30063d001105b10471b7a125c3fc
aHasnat1997 <hasnat56703@gmail.com>
2023-05-11 02:01:49 +0600

    get method done

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
 })
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rhfzczw.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@chocolate-management.r9to089.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,16 +23,38 @@ const client = new MongoClient(uri, {
   }
 });
 
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const chocolateCollection = client.db("chocolateDB").collection("chocolateInfo");
+
+
+    app.get('/chocolates', async (req, res) => {
+      const cursor = chocolateCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/chocolates', async (req, res) => {
+      const newChocolate = req.body;
+      console.log(newChocolate);
+      const result = await chocolateCollection.insertOne(newChocolate);
+      res.send(result);
+    })
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
